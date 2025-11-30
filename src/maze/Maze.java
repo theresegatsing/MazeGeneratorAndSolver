938aa3419c1d360c055119a9f0e8fda1d9f16343
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2025-11-30 17:44:29 -0500

    Initliased constructor

--- src/maze/Maze.java
+++ src/maze/Maze.java
@@ -6,5 +6,22 @@ package maze;
  * @author Therese Elvira Mombou Gatsing
  */
 public class Maze {
-
+	
+	private int length;
+	private int width;
+	
+	public Maze(int length, int width) {
+		this.length = length;
+		this.width = width;
+		
+	}
+	
+	
+	public int getLength() {
+		return this.length;
+	}
+	
+	public int getWidth() {
+		return this.width;
+	}
 }
