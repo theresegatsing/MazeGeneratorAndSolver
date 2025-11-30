44e76982e8abf9fd7f94bbb012e59da599a48524
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2025-11-30 18:54:32 -0500

    Added description

--- src/maze/MazeGenerator.java
+++ src/maze/MazeGenerator.java
@@ -1,11 +1,13 @@
 package maze;
 
 /**
- * 
+ * Randomly creates a maze based on the size provided by the user
  * 
  * @author Therese Elvira Mombou Gatsing
  */
 
 public class MazeGenerator {
+	
+	
 
 }
