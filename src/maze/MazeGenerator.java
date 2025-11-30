8b7cb7fd3a4fe68ca0f6c5c56f58292f6df4441e
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2025-11-30 18:56:07 -0500

    Added method to generate the maze

--- src/maze/MazeGenerator.java
+++ src/maze/MazeGenerator.java
@@ -1,4 +1,5 @@
 package maze;
+import java.util.Random;
 
 /**
  * Randomly creates a maze based on the size provided by the user
@@ -8,6 +9,37 @@ package maze;
 
 public class MazeGenerator {
 	
+	 private static final Random rand = new Random();
+
+	 public static Maze generate(int rows, int cols) {
+	        if (rows % 2 == 0) rows++;
+	        if (cols % 2 == 0) cols++;
+
+	        char[][] grid = new char[rows][cols];
+
+	        // 1. Fill with walls
+	        for (int r = 0; r < rows; r++) {
+	            for (int c = 0; c < cols; c++) {
+	                grid[r][c] = '#';
+	            }
+	        }
+
+	        // 2. Choose a start cell (odd, odd)
+	        int startRow = 1;
+	        int startCol = 1;
+	        grid[startRow][startCol] = ' ';
+
+	        // 3. Carve passages recursively
+	        carve(grid, startRow, startCol);
+
+	        // 4. Set start and end points
+	        grid[startRow][startCol] = 'S';
+	        int endRow = rows - 2;
+	        int endCol = cols - 2;
+	        grid[endRow][endCol] = 'E';
+
+	        return new Maze(grid, startRow, startCol, endRow, endCol);
+	    }
 	
 
 }
