28a701c41c2c75e779e68d72969ac4a545458f24
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2025-11-30 19:03:03 -0500

    Added method to solve the maze

--- src/maze/MazeSolver.java
+++ src/maze/MazeSolver.java
@@ -7,5 +7,16 @@ package maze;
  */
 
 public class MazeSolver {
+	
+    public static boolean solve(Maze maze) {
+        char[][] grid = maze.getGrid();
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+
+        int sr = maze.getStartRow();
+        int sc = maze.getStartCol();
+
+        // We don't overwrite 'S' and 'E' themselves, only the path between them
+        return dfs(grid, visited, sr, sc);
+    }
 
 }
